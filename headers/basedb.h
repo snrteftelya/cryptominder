6f06e59a6939b55b373f6302edc4f47e65557621
Николай Ковалев <snrteftelya@MacBook-Pro-Nikolaj.local>
2024-11-26 02:22:51 +0300

    fix sonar #3

--- headers/basedb.h
+++ headers/basedb.h
@@ -6,14 +6,13 @@
 #include "Account.h"
 
 class BaseDatabase {
-protected:
-    pqxx::connection *conn;
+public:
+    std::unique_ptr<pqxx::connection> conn;
     std::unique_ptr<Account> account;
 
-public:
-    BaseDatabase() {
-        conn = new pqxx::connection("dbname=crypto user=crypto_owner password=I6XNyohUfBj8 host=ep-yellow-truth-a2dw0siz.eu-central-1.aws.neon.tech sslmode=require");
-    }
+    BaseDatabase()
+        : conn(std::make_unique<pqxx::connection>("dbname=crypto user=crypto_owner password=I6XNyohUfBj8 host=ep-yellow-truth-a2dw0siz.eu-central-1.aws.neon.tech sslmode=require"))
+    {}
 
     virtual ~BaseDatabase() = default;
 
@@ -21,7 +20,7 @@ public:
         account = std::make_unique<Account>(name, username, password, *conn);
     }
 
-    pqxx::connection* getConnection() { return conn; }
+    pqxx::connection* getConnection() { return conn.get(); }
     Account* getAccount() { return account.get(); }
 };
 
